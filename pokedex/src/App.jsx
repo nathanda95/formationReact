4605a774536a7348e7a0d3f3e54d0d49af93d460
nathanda95 <nathandaligault95@gmail.com>
2024-07-13 08:02:34 +0200

    [feat]: remove onClick event and add condition

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,5 +1,6 @@
 const title = "Bonjour les <strong>gens</strong>"
 const style = {color: 'red', backgroundColor: 'blue'}
+const showTitle = false
 
 function App() {
 
@@ -8,7 +9,13 @@ function App() {
   }
 
   return <>
-    <h1 onClick={handleClick} id="title" className="title" style={style}>{title}</h1>
+    {
+      showTitle ? 
+      <h1 id="title" className="title" style={style}>
+        {title}
+      </h1> : 
+      <p>demo</p>
+    }
     <input type="text"/>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum esse possimus reprehenderit consequatur dolorem cupiditate cum tempora commodi a cumque tempore, fugit voluptatem, minima in saepe quia nihil dignissimos. Distinctio.
